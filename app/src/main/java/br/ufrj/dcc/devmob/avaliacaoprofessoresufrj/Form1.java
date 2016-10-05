eb75af4c1bee3f98d85aedce731f984c6e5ac5f4
Pedro Kleiz <pkleiz@hotmail.com>
2016-10-04 22:12:48 -0300

    Mudanças 04/10
    Melhoria no designer, utilização de hint, escolha do formulário baseado
    na pesquisa, estrelas agr tem valores(funçao pode ser usada para as
    outras), podem ser passadas como parametros.

--- app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Form1.java
+++ app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Form1.java
@@ -1,20 +1,35 @@
 package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Form1 extends AppCompatActivity {
+import static android.R.attr.rating;
+
+public class Form1 extends Activity {
 
     TextView txt_mat;
     Button btn_prox;
+    RatingBar rtg_1;
+    RatingBar rtg_2;
+    RatingBar rtg_3;
+    TextView result_1;
+    TextView result_2;
+    TextView result_3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form1);
+
+        mostraValor();
 
         txt_mat = (TextView)findViewById(R.id.txt_mat);
         btn_prox = (Button)findViewById(R.id.btn_prox);
@@ -24,5 +39,29 @@ public class Form1 extends AppCompatActivity {
             String n = b.getString("NOME");
             txt_mat.setText(n);
         }
+
+                btn_prox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), "Sua resposta foi salva", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+
     }
+    public void mostraValor()
+    {
+        rtg_1= (RatingBar) findViewById(R.id.ratingBar);
+        result_1= (TextView) findViewById(R.id.Result1_ID);
+        //quando o valor é mudado então muda n na tela, isso só serve pra mostrar que os valores já não são somente estrelas
+        //já podemos trabalhar com esses valores, armazenando-os em variáveis
+        rtg_1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar rtg_1, float rating, boolean fromUser) {
+                result_1.setText(String.valueOf(rating));
+            }
+        });
+    }
+
+
+
 }
