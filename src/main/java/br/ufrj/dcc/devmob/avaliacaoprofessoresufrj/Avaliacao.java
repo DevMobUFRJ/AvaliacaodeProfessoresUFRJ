25bec5fe896ddf0a29aac7c5d2174356a2c1b2b1
Gabriel Felipe Vargas Ferreira <gabrielfvf@outlook.com>
2017-03-24 16:52:16 -0300

    Atualização da tela de Avaliação
    Atualizada a tela de avaliação, agora com melhor design junto de todos os botões necessários;
    Criado uma classe que será usara para pegar os valores da tela de Avaliação que serão usados no BD;
    Modificado o Form1 conforme o necessário para a atualização.

---
+++ src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Avaliacao.java
@@ -0,0 +1,85 @@
+package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj;
+
+/**
+ * Created by devmob on 24/03/17.
+ */
+
+public class Avaliacao{
+
+    private int id;
+    private String dreAluno;
+    private String matProf;
+    private String comentario;
+    private float nota;
+    private String data;
+    private boolean status;
+    private int[] avaliacao;
+
+
+    public float setNota(float fNota){
+
+        nota = fNota;
+        return nota;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDreAluno() {
+        return dreAluno;
+    }
+
+    public void setDreAluno(String dreAluno) {
+        this.dreAluno = dreAluno;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int[] getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int[] avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String getMatProf() {
+        return matProf;
+    }
+
+    public void setMatProf(String matProf) {
+        this.matProf = matProf;
+    }
+}
