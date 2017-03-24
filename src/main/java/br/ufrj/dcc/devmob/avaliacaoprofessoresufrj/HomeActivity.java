662c1214a75a0c367b3735df039872453ebb3c4c
Gabriel Felipe Vargas Ferreira <gabrielfvf@outlook.com>
2017-03-24 09:56:41 -0300

    Tela de Avaliação Atualizada
    Retirada das estrelas, deixando apenas uma para avaliação geral;
    Adicionado o botão teste de avaliação, onde cada clique a torna positiva, negativa ou neutra;
    Adicionado também um toast específico para cada tipo de avaliação, informando o usuário da ação feita por ele.

---
+++ src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/HomeActivity.java
@@ -0,0 +1,56 @@
+package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button zimbraoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        zimbraoButton = (Button)findViewById(R.id.button);
+        zimbraoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button:
+                Intent i = new Intent(this, Form1.class);
+                i.putExtra("NOME", zimbraoButton.getText().toString());
+                startActivity(i);
+                break;
+
+        }
+    }
+}
