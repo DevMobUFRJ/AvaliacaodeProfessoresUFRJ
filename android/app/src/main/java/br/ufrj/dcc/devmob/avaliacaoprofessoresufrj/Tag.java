ec4669fdf25c7e272c08606a6e4c20096b814422
georgerappel <george.concei@hotmail.com>
2017-04-04 10:44:06 -0300

    Adicionar classe Tag

--- android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Tag.java
+++ android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Tag.java
@@ -5,4 +5,22 @@ package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj;
  */
 
 public class Tag {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
