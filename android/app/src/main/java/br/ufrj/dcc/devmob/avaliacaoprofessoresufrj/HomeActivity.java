1eb9a2f12e3e0b04e01edfbd92c9f103efcd3075
Pedro Kleiz <pkleiz@hotmail.com>
2017-04-06 11:11:15 -0300

    fix

--- android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/HomeActivity.java
+++ android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/HomeActivity.java
@@ -1,56 +1,48 @@
 package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+import java.lang.reflect.Array;
 
-    Button zimbraoButton;
+public class HomeActivity extends Activity{
+
+    private ListView tela;
+
+    //itens que ficarão na tela
+    private String Itens[]={"Joao - Computacao","Mario Ricardo - Geo. Analitica", "Geovana - História da Arte","Joao - Computacao","Mario Ricardo - Geo. Analitica", "Geovana - História da Arte", "Joao - Computacao","Mario Ricardo - Geo. Analitica", "Geovana - História da Arte" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        zimbraoButton = (Button)findViewById(R.id.button);
-        zimbraoButton.setOnClickListener(this);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
+        tela = (ListView) findViewById(R.id.LV_tela);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        ArrayAdapter<String> adaptador=new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                Itens);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.button:
-                Intent i = new Intent(this, Form1.class);
-                i.putExtra("NOME", zimbraoButton.getText().toString());
-                startActivity(i);
-                break;
-
-        }
+        tela.setAdapter(adaptador);
+        tela.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                int codigotela=position;
+                String valorclicado= tela.getItemAtPosition(codigotela).toString();
+                Toast.makeText(getApplicationContext(),valorclicado,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
