366d7709421d7837caae31d92dcb5b6cd4e6511e
George Rappel <george.concei@hotmail.com>
2017-04-27 10:12:03 -0300

    Usar configurações globais de tag

--- android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/avaliacao/Avaliacao.java
+++ android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/avaliacao/Avaliacao.java
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
 
+import br.ufrj.dcc.devmob.avaliacaoprofessoresufrj.Utils;
+
 /**
  * Created by devmob on 24/03/17.
  */
@@ -18,7 +20,10 @@ public class Avaliacao{
     private float nota;
     private Date data;
     private boolean visivel;
-    private int[] tags;
+    /**
+     * Conferir Utils.tags_nomes
+     */
+    private int[] tags = new int[Utils.tags_count];
     private int dit_valor;
 
     public Avaliacao(){
@@ -108,17 +113,19 @@ public class Avaliacao{
         return tags;
     }
 
-    public void setTags(int[] tags) {
-        this.tags = tags;
+    /**
+     * Define o valor para cada tag específicada.
+     * @param tag tag cujo valor deve ser alterado
+     * @param valor 1, 0 ou -1, avaliacao da tag especificada.
+     * @return {@code true} se atualizar o valor corretamente. {@code false} se o Valor for inválido.
+     */
+    public boolean setTag(Utils.tag_docente tag, int valor){
+        if(valor > 1 || valor < -1)
+            return false;
+        tags[tag.getValue()] = valor;
+        return true;
     }
 
-    public void addTag(int tagId){
-        int[] aux = tags.clone();
-        tags = new int[aux.length + 1];
-        for(int i = 0; i < aux.length; i++)
-            tags[i] = aux[i];
-        tags[aux.length + 1] = tagId;
-    }
 
     public String getMatProf() {
         return matProf;
