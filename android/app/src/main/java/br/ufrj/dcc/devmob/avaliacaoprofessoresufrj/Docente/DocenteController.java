29bbd3ea65b983fa3e6cee05386dd41ecbe78a3e
gabrielfvf7 <gabrielfvf@outlook.com>
2017-08-21 12:28:00 -0300

    Adicionado arquivo DocenteController

---
+++ android/app/src/main/java/br/ufrj/dcc/devmob/avaliacaoprofessoresufrj/Docente/DocenteController.java
@@ -0,0 +1,25 @@
+package br.ufrj.dcc.devmob.avaliacaoprofessoresufrj.Docente;
+
+import java.util.List;
+import br.ufrj.dcc.devmob.avaliacaoprofessoresufrj.avaliacao.Avaliacao;
+import br.ufrj.dcc.devmob.avaliacaoprofessoresufrj.service.HttpService;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Gabriel on 17/08/2017.
+ */
+
+public class DocenteController  {
+
+    public static Call<List<Docente>> buscarDocente(String dc) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(HttpService.baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        HttpService service = retrofit.create(HttpService.class);
+        return service.buscarDocente(dc);
+    }
+}
